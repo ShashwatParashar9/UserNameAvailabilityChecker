3e9d21a3e1eb80abaf56aef151b62ea04e5eb02a
shashwat <gshashwatparashar@gmail.com>
2026-03-04 10:11:07 +0530

    Base application setup

---
+++ src/usernameavailabilitychecker.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+    static class UsernameChecker {
+
+        HashMap<String,Integer> users = new HashMap<>();
+        HashMap<String,Integer> attempts = new HashMap<>();
+
+    }
+
+    public static void main(String[] args) {
+
+        UsernameChecker system = new UsernameChecker();
+        System.out.println("Username Checker Started");
+
+    }
+}
